a0e686f4e571cdce79d5ac2b51fb0fe87c3bf487
rvasanth11 <131972758+rvasanth11@users.noreply.github.com>
2024-07-27 12:34:29 +0530

    Initial Commit

---
+++ MovieDatabaseApp/MovieDatabaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDatabaseAppApp.swift
+//  MovieDatabaseApp
+//
+//  Created by vasanth on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieDatabaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
